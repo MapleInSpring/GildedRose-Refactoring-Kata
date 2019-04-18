d4ba36034065f73e1f2d188ea631550a36624a6d
Ben Bowes <bowesemails@gmail.com>
2019-04-18 09:53:41 +0800

    Add basic test cases for normal items

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -5,15 +5,45 @@ import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    @Test fun sellinDecreases_normalItems() {
+        val items = arrayOf(Item("foo", 10, 0))
         val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        app.updateQuality()
+
+        assertEquals(9, app.items[0].sellIn)
     }
 
+    @Test
+    fun qualityDecreases_normalItems() {
+        val items = arrayOf(Item("foo", 10, 20))
+        val app = GildedRose(items)
 
+        app.updateQuality()
+
+        assertEquals(19, app.items[0].quality)
+    }
+
+    @Test
+    fun qualityDecreasesTwiceAsFastAfterSellInDatePassed_normalItems() {
+        val items = arrayOf(Item("foo", 0, 10))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(8, app.items[0].quality)
+    }
+
+    @Test
+    fun qualityIsNeverNegative() {
+        val items = arrayOf(Item("foo", 10, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+
+        assertEquals(0, app.items[0].quality)
+    }
 }
 
 
